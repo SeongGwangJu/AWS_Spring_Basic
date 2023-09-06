0935970eee45525b1727d8c59d964b2fbbe48264
SeongGwangJu <seonggwangdev@gmail.com>
2023-09-06 12:43:53 +0900

    Before Lunch, Studied about RestController, Controller, and RestAPI

--- src/main/java/com/aws/spring_study/SpringStudyApplication.java
+++ src/main/java/com/aws/spring_study/SpringStudyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringStudyApplication {
-
+//Git Test!
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStudyApplication.class, args);
 	}
--- src/main/java/com/aws/spring_study/controller/ReqStudyController.java
+++ src/main/java/com/aws/spring_study/controller/ReqStudyController.java
@@ -1,4 +1,34 @@
 package com.aws.spring_study.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class ReqStudyController {
+
+    @GetMapping("/test/get")
+    public Object get() {
+        Map<String, String> dataMap = new HashMap<>();
+        dataMap.put("name", "주성광");
+        dataMap.put("email", "aaa@gmail.com");
+        return dataMap;
+    }
+
+    @PostMapping("/test/post")
+    public Object post() {
+        return  null;
+    }
+
+    @PutMapping("/test/put")
+    public Object put() {
+        return "PUT";
+    }
+
+    @DeleteMapping("/test/delete")
+    public Object delete() {
+        return "DELETE";
+    }
 }
