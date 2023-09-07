242275a280701b29dae5c69d30913d5d0fd022bc
SeongGwangJu <seonggwangdev@gmail.com>
2023-09-07 17:27:50 +0900

    privata data 보호를 위해 application.yml file을 gitignore 추가 커밋

--- src/main/java/com/aws/spring_study/controller/UsersController.java
+++ src/main/java/com/aws/spring_study/controller/UsersController.java
@@ -1,6 +1,8 @@
 package com.aws.spring_study.controller;
 
 import com.aws.spring_study.controller.dto.RegisterUserReqDto;
+import com.aws.spring_study.repository.UserMappers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,10 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UsersController {
 
+	@Autowired
+	private UserMappers userMappers;
+
 	@CrossOrigin
 	@PostMapping("/user")
 	public ResponseEntity<?> registerUser(@RequestBody RegisterUserReqDto registerUserReqDto) {
-		System.out.println(registerUserReqDto);
+
+		Integer count = userMappers.saveUser(registerUserReqDto);
+
 		return ResponseEntity.ok().body(null);
 	}
 }
