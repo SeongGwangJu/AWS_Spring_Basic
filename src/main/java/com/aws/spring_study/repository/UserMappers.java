4e06356131f6c5d9dbeb01bc477124dea1a1841c
SeongGwangJu <seonggwangdev@gmail.com>
2023-09-07 17:31:15 +0900

    이미 커밋된 파일의 git ignore 적용 확인

--- src/main/java/com/aws/spring_study/repository/UserMappers.java
+++ src/main/java/com/aws/spring_study/repository/UserMappers.java
@@ -5,6 +5,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMappers {
-	public Integer saveUser(RegisterUserReqDto registerUserReqDto);
+	public int saveUser(RegisterUserReqDto registerUserReqDto);
 
 }
